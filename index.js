5106bb01bc3f03df4645e6b54200c69df20c9c59
Adam Gehring <geh12001@byui.edu>
2018-03-19 08:46:07 -0600

    Postage

--- index.js
+++ index.js
@@ -1,18 +1,59 @@
-const express = require('express')
+var cool = require('cool-ascii-faces');
+var express = require('express');
+var app = express();
 const path = require('path')
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/math', calculate)
+  .get('/getRate', handleMath)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 
-
+  function handleMath(request, response) {
+    var requestUrl = url.parse(request.url, true);
+  
+    console.log("Query parameters: " + JSON.stringify(requestUrl.query));
+  
+    // TODO: Here we should check to make sure we have all the correct parameters
+  
+    var operation = requestUrl.query.operation;
+    var operand1 = Number(requestUrl.query.operand1);
+    var operand2 = Number(requestUrl.query.operand2);
+  
+    computeOperation(response, operation, operand1, operand2);
+  }
+  
+  function computeOperation(response, op, left, right) {
+    op = op.toLowerCase();
+  
+    var result = 0;
+  
+    if (op == "add") {
+      result = left + right;
+    } else if (op == "subtract") {
+      result = left - right;		
+    } else if (op == "multiply") {
+      result = left * right;
+    } else if (op == "divide") {
+      result = left / right;
+    } else {
+      // It would be best here to redirect to an "unknown operation"
+      // error page or something similar.
+    }
+  
+    // Set up a JSON object of the values we want to pass along to the EJS result page
+    var params = {operation: op, left: left, right: right, result: result};
+  
+    // Render the response, using the EJS page "result.ejs" in the pages directory
+    // Makes sure to pass it the parameters we need.
+    response.render('pages/result', params);
+  
+  }
 
   function calculate(req,res){
   

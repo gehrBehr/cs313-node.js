0fad90682aed46e12398b2da0b3b9ebaa0e6682f
Adam Gehring <geh12001@byui.edu>
2018-03-19 11:12:25 -0600

    postage logic

--- index.js
+++ index.js
@@ -22,32 +22,87 @@ express()
     // TODO: Here we should check to make sure we have all the correct parameters
   
     var postageType = requestUrl.query.mailType;
-    var operand1 = Number(requestUrl.query.operand1);
-    var operand2 = Number(requestUrl.query.operand2);
+    var weight = Number(requestUrl.query.weight);
   
-    computeOperation(response, operation, operand1, operand2);
+    computeOperation(response, postageType, weight);
   }
   
-  function computeOperation(response, op, left, right) {
-    op = op.toLowerCase();
+  function computeOperation(response, type, weight) {
+    type = type.toLowerCase();
   
-    var result = 0;
-  
-    if (op == "add") {
-      result = left + right;
-    } else if (op == "subtract") {
-      result = left - right;		
-    } else if (op == "multiply") {
-      result = left * right;
-    } else if (op == "divide") {
-      result = left / right;
-    } else {
-      // It would be best here to redirect to an "unknown operation"
-      // error page or something similar.
+    var price = 0;
+
+    switch (type) {
+      case "stamped":
+        if(weight <= 1){
+          price = 0.50;
+        }else if(weight > 1 && weight <= 2){
+          price = 0.71;
+        }else if(weight > 2 && weight <= 3){
+          price = 0.92;
+        }else if(weight > 3 && weight <= 3.5){
+          price = 1.13;
+        }
+        else{
+          type = flat;
+        }
+      case "metered":
+        if(weight <= 1){
+          price = 0.47;
+        }else if(weight > 1 && weight <= 2){
+          price = 0.68;
+        }else if(weight > 2 && weight <= 3){
+          price = 0.89;
+        }else if(weight > 3 && weight <= 3.5){
+          price = 1.10;
+        }
+        else{
+          type = flat;
+        }
+      case "flat":
+        if(weight <= 4){
+          price = 3.50;
+        }else if(weight > 4 && weight <= 8){
+          price = 3.75;
+        }else if(weight > 8 && weight <= 9){
+          price = 4.10;
+        }else if(weight > 9 && weight <= 10){
+          price = 4.45;
+        }else if(weight > 10 && weight <= 11){
+          price = 4.80;
+        }else if(weight > 11 && weight <= 12){
+          price = 5.15;
+        }else if(weight > 12 && weight <= 13){
+          price = 5.50;
+        }else{
+          price = -1;
+        }      
+        break;
+      case "firstClass":
+        if(weight <= 4){
+          price = 3.50;
+        }else if(weight > 4 && weight <= 8){
+          price = 3.75;
+        }else if(weight > 8 && weight <= 9){
+          price = 4.10;
+        }else if(weight > 9 && weight <= 10){
+          price = 4.45;
+        }else if(weight > 10 && weight <= 11){
+          price = 4.80;
+        }else if(weight > 11 && weight <= 12){
+          price = 5.15;
+        }else if(weight > 12 && weight <= 13){
+          price = 5.50;
+        }else{
+          price = -1;
+        }   
+        break;  
+      default:
+        break;
     }
   
     // Set up a JSON object of the values we want to pass along to the EJS result page
-    var params = {operation: op, left: left, right: right, result: result};
+    var params = {type: type, weight: weight, price: price};
   
     // Render the response, using the EJS page "result.ejs" in the pages directory
     // Makes sure to pass it the parameters we need.

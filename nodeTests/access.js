b0fecc0ad4fdd0ddb05d8a027ff86acd23330e09
gehrBehr <gehringadam@byui.edu>
2018-03-31 18:37:58 -0600

    connect string

--- nodeTests/access.js
+++
@@ -1,9 +0,0 @@
-const { Client } = require('pg');
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
-
-const express = require('express')

e2e8f5b4992e4738eb91cd559ab6484e5caa8f93
gehrBehr <gehringadam@byui.edu>
2018-03-31 18:33:26 -0600

    connect

--- elearn/access.js
+++ elearn/access.js
@@ -1,5 +1,7 @@
-const { Client } = require('pg');
+const { Client } = require('mongodb').MongoClient;
 const { Pool } = require('pg');
+
+var connectionString = 'mongodb://heroku_n7fqkhx9:nn6ao650go64njqku8nld8torq@ds125068.mlab.com:25068/heroku_n7fqkhx9';
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,

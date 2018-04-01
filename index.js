ecfcc3617ee9fa069f83ef64f82cbf66b7fff20d
Adam Gehring <geh12001@byui.edu>
2018-03-31 18:47:44 -0600

    reconnect?

--- index.js
+++ index.js
@@ -1,148 +1,94 @@
-var express = require('express');
+const express = require('express');
+const path = require('path');
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local'),Strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var http = require('http');
+
+// var routes = require('./routes/index');
+// var users = require('./routes/users');
+
 var app = express();
-var url = require('url');
 
-app.set('port', (process.env.PORT || 5000));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(__dirname + '/public'));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+//express session
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: true,
+  resave: true
 
-app.get('/getRate', function(request, response) {
-	handleMath(request, response);
+}));
+
+//passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+//connect flash
+app.use(flash());
+
+//global vars
+app.use(function  (req, res, next){
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+})
+
+
+const PORT = process.env.PORT || 3000
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+client.connect();
 
-  function handleMath(request, response) {
-    var requestUrl = url.parse(request.url, true);
-  
-    console.log("Query parameters: " + JSON.stringify(requestUrl.query));
-  
-    // TODO: Here we should check to make sure we have all the correct parameters
-  
-    var postageType = requestUrl.query.mailType;
-    var weight = Number(requestUrl.query.weight);
-  
-    computeOperation(response, postageType, weight);
-  }
-  
-  function computeOperation(response, type, weight) {
-    type = type.toLowerCase();
-  
-    var price = 0;
+// app.use('/', routes);
+// app.use('/users', users);
+// app.use(express.static('views'));
 
-    switch (type) {
-      case "stamped":
-        if(weight <= 1){
-          price = 0.50;
-        }else if(weight > 1 && weight <= 2){
-          price = 0.71;
-        }else if(weight > 2 && weight <= 3){
-          price = 0.92;
-        }else if(weight > 3 && weight <= 3.5){
-          price = 1.13;
-        }
-        else{
-          type = flat;
-        }
-      case "metered":
-        if(weight <= 1){
-          price = 0.47;
-        }else if(weight > 1 && weight <= 2){
-          price = 0.68;
-        }else if(weight > 2 && weight <= 3){
-          price = 0.89;
-        }else if(weight > 3 && weight <= 3.5){
-          price = 1.10;
-        }
-        else{
-          type = flat;
-        }
-      case "flat":
-        if(weight <= 4){
-          price = 3.50;
-        }else if(weight > 4 && weight <= 8){
-          price = 3.75;
-        }else if(weight > 8 && weight <= 9){
-          price = 4.10;
-        }else if(weight > 9 && weight <= 10){
-          price = 4.45;
-        }else if(weight > 10 && weight <= 11){
-          price = 4.80;
-        }else if(weight > 11 && weight <= 12){
-          price = 5.15;
-        }else if(weight > 12 && weight <= 13){
-          price = 5.50;
-        }else{
-          price = -1;
-        }      
-        break;
-      case "firstClass":
-        if(weight <= 4){
-          price = 3.50;
-        }else if(weight > 4 && weight <= 8){
-          price = 3.75;
-        }else if(weight > 8 && weight <= 9){
-          price = 4.10;
-        }else if(weight > 9 && weight <= 10){
-          price = 4.45;
-        }else if(weight > 10 && weight <= 11){
-          price = 4.80;
-        }else if(weight > 11 && weight <= 12){
-          price = 5.15;
-        }else if(weight > 12 && weight <= 13){
-          price = 5.50;
-        }else{
-          price = -1;
-        }   
-        break;  
-      default:
-        break;
-    }
+// view engine setup
+app.use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .engine('handlebars', exphbs({defaultLayout:'layout'}))
+  .set('view engine', 'handlebars')
+  .get('/', function(req,res){
+    res.render('index.handlebars');
+  })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
   
-    // Set up a JSON object of the values we want to pass along to the EJS result page
-    var params = {type: type, weight: weight, price: price};
-  
-    // Render the response, using the EJS page "result.ejs" in the pages directory
-    // Makes sure to pass it the parameters we need.
-    response.render('pages/result', params);
-  
-  }
 
-  function calculate(req,res){
-  
-    var Operand_one = req.query.Operand_one;
-    var Operand_two = req.query.Operand_two;
-    var operator = req.query.operator;
-
-    one = parseInt(Operand_one);
-    two = parseInt(Operand_two);
-
-    var result = 0;
-
-    switch (operator) {
-      case "add":
-       result = one + two; 
-        break;
-      case "sub":
-       result = one - two; 
-        break;
-      case "mult":
-      result = one * two; 
-        break;
-      case "divi":
-      result = one / two; 
-        break;  
-      default:
-      result = -1;
-        break;
-    }
-
-    console.log(result);
-
-  }

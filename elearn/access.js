f32f772ea9c02e06c7aea051ab4dd534fbbc9c29
gehrBehr <gehringadam@byui.edu>
2018-04-04 21:33:33 -0600

    added button to index

--- elearn/access.js
+++ elearn/access.js
@@ -1,11 +1,6 @@
 const { Client } = require('mongodb').MongoClient;
-const { Pool } = require('pg');
 
 var connectionString = 'mongodb://heroku_n7fqkhx9:nn6ao650go64njqku8nld8torq@ds125068.mlab.com:25068/heroku_n7fqkhx9';
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
 
 const express = require('express')
